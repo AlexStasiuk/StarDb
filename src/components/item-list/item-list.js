b4b91de711b8476fcf4f169985129a270e9ca317
AlexStasiuk <sov853972@gmail.com>
2021-08-16 23:09:16 +0300

    activate item-list fix bugs in swapi-service and fix all warnings

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 
 import './item-list.css';
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
 
 export default class ItemList extends Component {
 
+  swapiService = new SwapiService();
+
+  state = {
+    peopleList: null
+  }
+  componentDidMount(){
+    this.swapiService
+      .getAllPeople()
+      .then((peopleList) => {
+        this.setState({
+          peopleList
+        });
+      });
+  };
+
   render() {
+
+    const {peopleList} = this.state;
+
+    if(!peopleList){
+      return(
+      <Spinner />);
+    }
+    const peopleListItems = peopleList.map(({id, name}) =>{
+      return(
+      <li 
+        key = {id}
+        className = "list-group-item">
+        {name}
+      </li>);
+    });
+
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
+        {peopleListItems}
       </ul>
     );
   }
